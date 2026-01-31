3b65458289f39def0a173279ff6b1bf0f0602779
adithya <adithyasivaraman5@gmail.com>
2026-01-31 12:05:17 +0530

    [Adithya] Added : Coded UC2 to Render OOPS as Banner using Print Statements

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,11 +1,23 @@
 /**
- * OOPS Banner Application
- * @author Adi
- * @version 1.0
+ * OOPSBannerApp UC2 – OOPS Banner Display Application
+ *
+ * This class renders the word OOPS in a banner format
+ * using print statements.
+ *
+ * @author Developer
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  *****     *****   ******     ****** ");
+        System.out.println(" *     *   *     *  *     *   *       ");
+        System.out.println("*       * *       * *      *  *       ");
+        System.out.println("*       * *       * *     *    ****** ");
+        System.out.println("*       * *       * *  *             *");
+        System.out.println(" *     *   *     *  *                *");
+        System.out.println("  *****     *****   *          ****** ");
+
     }
 }
