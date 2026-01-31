98316d1433575fd11588b31562c548fd3be15dba
adithya <adithyasivaraman5@gmail.com>
2026-01-31 12:36:14 +0530

    [Adithya] Added : Coded UC3 to Render OOPS Banner using String.join()

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,23 +1,13 @@
-/**
- * OOPSBannerApp UC2 – OOPS Banner Display Application
- *
- * This class renders the word OOPS in a banner format
- * using print statements.
- *
- * @author Developer
- * @version 2.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("  *****     *****   ******     ****** ");
-        System.out.println(" *     *   *     *  *     *   *       ");
-        System.out.println("*       * *       * *      *  *       ");
-        System.out.println("*       * *       * *     *    ****** ");
-        System.out.println("*       * *       * *  *             *");
-        System.out.println(" *     *   *     *  *                *");
-        System.out.println("  *****     *****   *          ****** ");
-
+        System.out.println(String.join(" ", "   ***   ", "   ***   ", "******   ", " ******* "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", "**    ** ", "**       "));
+        System.out.println(String.join(" ", "**     **", "**     **", "**     **", "**       "));
+        System.out.println(String.join(" ", "**     **", "**     **", "**    ** ", " ******* "));
+        System.out.println(String.join(" ", "**     **", "**     **", "******   ", "       **"));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", "**       ", "       **"));
+        System.out.println(String.join(" ", "   ***   ", "   ***   ", "**       ", " ******* "));
     }
 }
